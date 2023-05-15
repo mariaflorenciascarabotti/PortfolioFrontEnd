ba38c49beb49fe8e5c08d79c4b68f573a6289140
Maria Florencia Scarabotti <florenciascarabotti@gmail.com>
2023-05-15 20:14:34 -0300

    creo component banner

--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
