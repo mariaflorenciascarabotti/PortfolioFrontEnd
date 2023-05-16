514435521b2b85d6c4909de36ba64f885b25e722
Maria Florencia Scarabotti <florenciascarabotti@gmail.com>
2023-05-16 01:46:07 -0300

    all my works

--- my-portfolio/src/app/components/works-done/works-done.component.ts
+++ my-portfolio/src/app/components/works-done/works-done.component.ts
@@ -8,12 +8,32 @@ import { Component, Input, OnInit } from '@angular/core';
 export class WorksDoneComponent implements OnInit {
   @Input() title:string = 'Trabajos';
 
-  work = {
-    "id": 1,
-    "title": "Primer trabajo",
-    "urlImage": "https://blog.educacionit.com/wp-content/uploads/2018/09/shutterstock-10338536170938-620x354-01.jpg",
-    "description": "Me encantó este trabajo"
-  }
+  workList = [
+    {
+      "id": 1,
+      "title": "Primer trabajo",
+      "urlImage": "https://blog.educacionit.com/wp-content/uploads/2018/09/shutterstock-10338536170938-620x354-01.jpg",
+      "description": "Me encantó este trabajo"
+    },
+    {
+      "id": 2,
+      "title": "Segundo trabajo",
+      "urlImage": "https://lh3.googleusercontent.com/XSDv6XYZ973bdxMBDJ1adLHpSSUv4vsZJaePpms21eZDl-27JIfTHIYXnnudwPfAg_1-59bKAarMhWGNagsTR2Gq0pAWBUw6CwYwH2V0TzDXZT9z1fKvr1vCOrk8nxZ-U7wVfOJq",
+      "description": "Me encantó este trabajo"
+    },
+    {
+      "id": 3,
+      "title": "Tercero trabajo",
+      "urlImage": "https://process.fs.teachablecdn.com/ADNupMnWyR7kCWRvm76Laz/resize=width:705/https://www.filepicker.io/api/file/X3O3nWaR16Wiw1vbloQq",
+      "description": "Me encantó este trabajo"
+    },
+    {
+      "id": 4,
+      "title": "Cuarto trabajo",
+      "urlImage": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCqscj-HDlmxCQm8BKNI3XLE-FU9o5d-HySDdNf1SdS19yL6wYpyExFTbUxsafCSNhcD0&usqp=CAU",
+      "description": "Me encantó este trabajo"
+    }
+];
 
   constructor() { }
 
